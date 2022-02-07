23d69fb6b599db2f439af6e5e00f84a724698774
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-02-07 12:16:18 +0300

    made chnages on interpolations

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,11 @@
 
-export class Quote {
-    showAuthor: boolean;
-    constructor(public id: number,public Author: string,public genre: string,public name:string,public completeDate: Date){
-      this.showAuthor=false;
+
+
+      export class Quote {
+        showAuthor: boolean;
+        constructor(public id: number,public Author: string,public genre: string,public name:string,public completeDate: Date){
+          this.showAuthor=false;
+    
 
 }
 }
